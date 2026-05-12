e57ae1a5d0c4cea494790e72872bc5b4d55e8b73
Spaggety <spaggwtywork@gmail.com>
2026-05-12 15:16:43 +0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div style={{ padding: 40, fontFamily: "sans-serif" }}>
       <h1>Telegram Bot is running 🤖</h1>
-      <p>Webhook endpoint: /api/telegram</p>
+      <p>Webhook хуюк: /api/telegram</p>
     </div>
   );
 }
