9d23a8e382afd35ab775cc0255d882551a0b9ebf
Spaggety <spaggwtywork@gmail.com>
2026-05-13 08:55:27 +0300

    fix

--- app/api/telegram/route.js
+++ app/api/telegram/route.js
@@ -68,8 +68,28 @@ export async function POST(req) {
         return cachedCsrf;
       }
 
-      const res = await fetch("https://passport.yandex.ru/pwl-yandex/auth/add?retpath=https%3A%2F%2Fid.yandex.ru%2F&noreturn=1&cause=auth&process_uuid=", {
+      const res = await fetch("https://passport.yandex.ru/pwl-yandex", {
+        headers: {
+          accept:
+            "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+          "accept-language": "ru",
+          "cache-control": "max-age=0",
+          priority: "u=0, i",
+          "sec-ch-prefers-color-scheme": "light",
+          "sec-ch-ua":
+            '"Chromium";v="148", "Google Chrome";v="148", "Not/A)Brand";v="99"',
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": '"Windows"',
+          "sec-fetch-dest": "document",
+          "sec-fetch-mode": "navigate",
+          "sec-fetch-site": "none",
+          "sec-fetch-user": "?1",
+          "upgrade-insecure-requests": "1",
+        },
+        body: null,
         method: "GET",
+        mode: "cors",
+        credentials: "include",
       });
 
       const html = await res.text();
