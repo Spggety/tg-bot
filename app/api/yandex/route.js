cc596b4f22e91997a26dfe16f68a1097f2866294
Spaggety <spaggwtywork@gmail.com>
2026-05-13 09:55:56 +0300

    fix

--- app/api/yandex/route.js
+++ app/api/yandex/route.js
@@ -1,36 +1,20 @@
-export async function GET() {
-  const res = await fetch("https://passport.yandex.ru/pwl-yandex", {
-    headers: {
-      referer: "https://passport.yandex.ru/",
-      accept:
-        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-      "accept-language": "ru",
-      "cache-control": "max-age=0",
-      priority: "u=0, i",
-      "sec-ch-prefers-color-scheme": "light",
-      "sec-ch-ua":
-        '"Chromium";v="148", "Google Chrome";v="148", "Not/A)Brand";v="99"',
-      "sec-ch-ua-mobile": "?0",
-      "sec-ch-ua-platform": '"Windows"',
-      "sec-fetch-dest": "document",
-      "sec-fetch-mode": "navigate",
-      "sec-fetch-site": "same-origin",
-      "sec-fetch-user": "?1",
-      "upgrade-insecure-requests": "1",
-    },
-    body: null,
-    method: "GET",
-  });
-  let html = await res.text();
+export async function GET(req) {
+  const target =
+    "https://passport.yandex.ru/pwl-yandex";
 
-  html = html.replace(
-    "<head>",
-    `<head><base href="https://passport.yandex.ru/">`,
-  );
+  const res = await fetch(target, {
+    headers: {
+      "user-agent":
+        req.headers.get("user-agent") || "",
+    },
+  });
+
+  const html = await res.text();
 
   return new Response(html, {
+    status: res.status,
     headers: {
-      "content-type": "text/html; charset=utf-8",
+      "content-type": "text/html",
     },
   });
 }
